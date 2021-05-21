afa38b394f8ace3a467ad709feb6dc47d20474de
PRASASTIRAI <77830876+PRASASTIRAI@users.noreply.github.com>
2021-05-21 11:23:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function addnew(){
+    window.location="web.html";
+    localStorage.getItem(user_name);
+ console.log("the User Name is :" + user_name);
+ localStorage.getItem(user_age);
+ console.log("the User Age is : " + user_age);
+   
+ }
